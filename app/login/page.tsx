bc368eab40c186305c0ee7d69d5498921229b13a
Daniel Ha <dannyha@Outlook.com>
2025-11-22 21:11:10 -0800

    updating login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from "react";
+import { login } from "../lib/actions/auth";
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="flex min-h-screen items-center justify-center">
         <Suspense>
-          <h2 className="text-2xl font-bold">You need login!</h2>
+          <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">You are not signed in</h1>
+            <button onClick={login}>Sign in with Spotify</button>
+          </div>
         </Suspense>
       </div>
     </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { login } from "./lib/actions/auth";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="flex min-h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold">You are not signed in</h1>
-        <button onClick={() => login()}>Sign in with Spotify</button>
-      </div>
-    </main>
-  );
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <main className="flex items-center justify-center md:h-screen">
+        <div className="flex min-h-screen items-center justify-center">
+          <h1 className="text-4xl font-bold">You are signed in</h1>
+        </div>
+      </main>
+    );
+  } else {
+    redirect("/login");
+  }
 }
