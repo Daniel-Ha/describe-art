7600e739d212257e8b8a1e6d35e52fe126048b82
Daniel Ha <dannyha@Outlook.com>
2025-11-22 21:24:19 -0800

    updating home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-"use client";
-
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
-export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <main className="flex items-center justify-center md:h-screen">
-        <div className="flex min-h-screen items-center justify-center">
-          <h1 className="text-4xl font-bold">You are signed in</h1>
-        </div>
-      </main>
-    );
-  } else {
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
     redirect("/login");
   }
+
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">You are signed in</h1>
+      </div>
+    </main>
+  );
 }
